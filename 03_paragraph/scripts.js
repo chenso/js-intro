30067fc9b2ae62148f5cc28737a4c4dbb8075ec6
chenso <chen.s@wustl.edu>
2016-04-11 18:20:35 -0500

    submis

---
+++ 03_paragraph/scripts.js
@@ -0,0 +1,9 @@
+document.write("<h1>h1 Title</h1>");
+
+function writeParagraph(paragraph) {
+    document.write("<p>"+ paragraph + "</p>");
+}
+
+writeParagraph("A JavaScript function is a block of code designed to perform a particular task. A JavaScript function is executed when 'something' invokes it (calls it).")
+writeParagraph("A JavaScript function is a block of code designed to perform a particular task. A JavaScript function is executed when 'something' invokes it (calls it).")
+writeParagraph("A JavaScript function is a block of code designed to perform a particular task. A JavaScript function is executed when 'something' invokes it (calls it).")
---
+++ 04_vars/scripts.js
@@ -0,0 +1,4 @@
+var a = 5;
+var b = 10;
+
+document.write("<h1>" + (a + b) + "</h1?");
